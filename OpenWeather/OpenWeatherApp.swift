904c27cd3a29b9099936492b11f31e9472546b53
Jael <jael.ruvalcaba@uabc.edu.mx>
2024-10-02 09:05:03 -0700

    Initial Commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by The Coding Kid on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
